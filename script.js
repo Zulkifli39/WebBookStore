7f1fdb03cb2634b68dcb5fa527a83c328381c624
zulkifli39 <mhadzulkffy@gmail.com>
2023-06-09 21:51:22 +0800

    WebBookStore

---
+++ script.js
@@ -0,0 +1,17 @@
+// Swiper 
+
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+   
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    on: {
+      autoplayTimeLeft(s, time, progress) {
+        progressCircle.style.setProperty("--progress", 1 - progress);
+        progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+      }
+    }
+  });
